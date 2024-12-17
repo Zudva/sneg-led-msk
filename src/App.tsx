607f312f82415fce5f38572673c54644633e4344
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2024-12-17 15:26:37 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+        <h1 className="text-3xl text-red-100 font-bold underline">
+            Hello world!
+        </h1>
+    </>
+  )
+}
+
+export default App
